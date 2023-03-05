b0f7ade87e7fd48bf07a50a1b96482ec5deba622
Anthony Quigley <aquigley1777@gmail.com>
2023-03-05 16:56:40 -0500

    Fixup formatting

--- src/main/java/com/aquigs/attackedtimer/AttackedLogoutTimerPlugin.java
+++ src/main/java/com/aquigs/attackedtimer/AttackedLogoutTimerPlugin.java
@@ -1,7 +1,9 @@
 package com.aquigs.attackedtimer;
 
 import com.google.inject.Provides;
+
 import javax.inject.Inject;
+
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
@@ -13,41 +15,36 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
-@PluginDescriptor(
-	name = "Attacked Logout Timer"
-)
-public class AttackedLogoutTimerPlugin extends Plugin
-{
-	@Inject
-	private Client client;
+@PluginDescriptor(name = "Attacked Logout Timer")
+public class AttackedLogoutTimerPlugin extends Plugin {
+    @Inject private Client client;
 
-	@Inject
-	private AttackedLogoutTimerConfig config;
+    @Inject private AttackedLogoutTimerConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Attacked Logout Timer started!");
-	}
+    @Override
+    protected void startUp() throws Exception {
+        log.info("Attacked Logout Timer started!");
+    }
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Attacked Logout Timer stopped!");
-	}
+    @Override
+    protected void shutDown() throws Exception {
+        log.info("Attacked Logout Timer stopped!");
+    }
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Attacked Logout Timer says " + config.greeting(), null);
-		}
-	}
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged gameStateChanged) {
+        if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
+            client.addChatMessage(
+                ChatMessageType.GAMEMESSAGE,
+                "",
+                "Attacked Logout Timer says " + config.greeting(),
+                null
+            );
+        }
+    }
 
-	@Provides
-	AttackedLogoutTimerConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(AttackedLogoutTimerConfig.class);
-	}
+    @Provides
+    AttackedLogoutTimerConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(AttackedLogoutTimerConfig.class);
+    }
 }
--- src/test/java/com/aquigs/attackedtimer/AttackedLogoutTimerPluginTest.java
+++ src/test/java/com/aquigs/attackedtimer/AttackedLogoutTimerPluginTest.java
@@ -3,11 +3,9 @@ package com.aquigs.attackedtimer;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class AttackedLogoutTimerPluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(AttackedLogoutTimerPlugin.class);
-		RuneLite.main(args);
-	}
+public class AttackedLogoutTimerPluginTest {
+    public static void main(String[] args) throws Exception {
+        ExternalPluginManager.loadBuiltin(AttackedLogoutTimerPlugin.class);
+        RuneLite.main(args);
+    }
 }
