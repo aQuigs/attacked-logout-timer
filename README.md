4dc63f099aeaa9dbeea66648e41f4084df76e58d
Anthony Quigley <aquigley1777@gmail.com>
2023-03-05 15:54:52 -0500

    Initial commit from RuneLite generate script

---
+++ README.md
@@ -0,0 +1,2 @@
+# Attacked Logout Timer
+RuneLite plugin for displaying a timer when you are attacked to count down until you are able to log out
---
+++ src/test/java/com/aquigs/attackedtimer/AttackedLogoutTimerPluginTest.java
@@ -0,0 +1,13 @@
+package com.aquigs.attackedtimer;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class AttackedLogoutTimerPluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(AttackedLogoutTimerPlugin.class);
+		RuneLite.main(args);
+	}
+}
